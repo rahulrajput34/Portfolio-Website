5a85a888157d04791f457e155100ca9b4e9907a5
Rahul Rajput <rajputrahul3424@gmail.com>
2025-07-31 15:22:33 -0400

    feat: add the header and hero section inside the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Header } from "@/sections/Header";
+import { HeroSection } from "@/sections/Hero";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Header />
+      <HeroSection />
     </div>
   );
 }
