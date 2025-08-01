2e8cee1b636e12a5a4913f45ce2480cbe60056d5
Rahul Rajput <rajputrahul3424@gmail.com>
2025-08-01 15:13:54 -0400

    feat: add the css classes to make the ui more perfect

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -8,46 +8,48 @@ import SparkleIcon from "@/assets/icons/sparkle.svg";
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-      <div
-        className="absolute inset-0 -z-30 opacity-5"
-        style={{
-          backgroundImage: `url(${grainImage.src})`,
-        }}
-      ></div>
-      <div className="size-[620px] hero-ring"></div>
-      <div className="size-[820] hero-ring"></div>
-      <div className="size-[1020] hero-ring"></div>
-      <div className="size-[1220] hero-ring"></div>
-      <HeroOrbit size={800} rotation={-72}>
-        <StarIcon className="text-emerald-300 size-28" />
-      </HeroOrbit>
-      <HeroOrbit size={550} rotation={20}>
-        <StarIcon className="text-emerald-300 size-12" />
-      </HeroOrbit>
-      <HeroOrbit size={590} rotation={98}>
-        <StarIcon className="text-emerald-300 size-8" />
-      </HeroOrbit>
-      <HeroOrbit size={430} rotation={-14}>
-        <SparkleIcon className="text-emerald-300/20 size-8" />
-      </HeroOrbit>
-      <HeroOrbit size={440} rotation={79}>
-        <SparkleIcon className="text-emerald-300/20 size-5" />
-      </HeroOrbit>
-      <HeroOrbit size={530} rotation={178}>
-        <SparkleIcon className="text-emerald-300/20 size-10" />
-      </HeroOrbit>
-      <HeroOrbit size={710} rotation={144}>
-        <SparkleIcon className="text-emerald-300/20 size-14" />
-      </HeroOrbit>
-      <HeroOrbit size={720} rotation={85}>
-        <div className="size-3 rounded-full bg-emerald-300/20"></div>
-      </HeroOrbit>
-      <HeroOrbit size={520} rotation={-41}>
-        <div className="size-3 rounded-full bg-emerald-300/20"></div>
-      </HeroOrbit>
-      <HeroOrbit size={650} rotation={-5}>
-        <div className="size-3 rounded-full bg-emerald-300/20"></div>
-      </HeroOrbit>
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
+          style={{
+            backgroundImage: `url(${grainImage.src})`,
+          }}
+        ></div>
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[820] hero-ring"></div>
+        <div className="size-[1020] hero-ring"></div>
+        <div className="size-[1220] hero-ring"></div>
+        <HeroOrbit size={800} rotation={-72}>
+          <StarIcon className="text-emerald-300 size-28" />
+        </HeroOrbit>
+        <HeroOrbit size={550} rotation={20}>
+          <StarIcon className="text-emerald-300 size-12" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={98}>
+          <StarIcon className="text-emerald-300 size-8" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotation={-14}>
+          <SparkleIcon className="text-emerald-300/20 size-8" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={79}>
+          <SparkleIcon className="text-emerald-300/20 size-5" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={178}>
+          <SparkleIcon className="text-emerald-300/20 size-10" />
+        </HeroOrbit>
+        <HeroOrbit size={710} rotation={144}>
+          <SparkleIcon className="text-emerald-300/20 size-14" />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotation={85}>
+          <div className="size-3 rounded-full bg-emerald-300/20"></div>
+        </HeroOrbit>
+        <HeroOrbit size={520} rotation={-41}>
+          <div className="size-3 rounded-full bg-emerald-300/20"></div>
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-5}>
+          <div className="size-3 rounded-full bg-emerald-300/20"></div>
+        </HeroOrbit>
+      </div>
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
