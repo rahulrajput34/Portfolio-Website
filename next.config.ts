8d1ae96a087d38ca2df7767f51386f7646f551b0
Rahul Rajput <rajputrahul3424@gmail.com>
2025-08-01 00:09:22 -0400

    fix:  add turbopack for solving the svg imports errors

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
